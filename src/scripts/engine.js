8b59f2dab7c7a7ef0f05f378e67f95266d1cbcbc
Joao Ferreira <yferreirajoao@gmail.com>
2024-12-01 16:11:30 -0300

    Import Sounds

---
+++ src/scripts/engine.js
@@ -0,0 +1,10 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+let audio = new Audio("src/tunes/a.wav");
+const playTune = (key) => {
+    audio.play();
+};
+
+pianoKeys.forEach((key) => {
+    console.log(key.dataset.key);
+    key.addEventListener("click", () => playTune(key.dataset.key));
+});

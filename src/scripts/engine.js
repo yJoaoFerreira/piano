c6d7d6dbdc0c479cf337f9eee0e3aaf5afc72c86
Joao Ferreira <yferreirajoao@gmail.com>
2024-12-01 16:14:13 -0300

    Fix Sounds

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,6 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 let audio = new Audio("src/tunes/a.wav");
 const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`;
     audio.play();
 };
 

d85d271473a2a60ac1946122265247049f45b920
Joao Ferreira <yferreirajoao@gmail.com>
2024-12-01 22:47:21 -0300

    Controle de erros

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,9 +1,11 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav");
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`;
     audio.play();
-
+    
     const clickedKey = document.querySelector(`[data-key="${key}"]`);
     clickedKey.classList.add("active");
     setTimeout(() => {
@@ -12,10 +14,12 @@ const playTune = (key) => {
 };
 
 pianoKeys.forEach((key) => {
-    console.log(key.dataset.key);
     key.addEventListener("click", () => playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
-    playTune(e.key);
+    if(mapedKeys.includes(e.key)){
+        playTune(e.key);
+    };
 });

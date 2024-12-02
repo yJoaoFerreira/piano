d511577edb3bb275bcabb2e38240d253618064bf
Joao Ferreira <yferreirajoao@gmail.com>
2024-12-01 21:45:01 -0300

    Keybind

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -9,3 +9,7 @@ pianoKeys.forEach((key) => {
     console.log(key.dataset.key);
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
+
+document.addEventListener("keydown", (e) => {
+    playTune(e.key);
+});

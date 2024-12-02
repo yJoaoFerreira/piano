50bbf4da629e2c936c643866a8236df451f11b68
Joao Ferreira <yferreirajoao@gmail.com>
2024-12-01 21:53:16 -0300

    hover nas keys feitas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,6 +3,12 @@ let audio = new Audio("src/tunes/a.wav");
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`;
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+       clickedKey.classList.remove("active"); 
+    }, 150);
 };
 
 pianoKeys.forEach((key) => {
